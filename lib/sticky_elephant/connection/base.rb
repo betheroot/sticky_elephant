63ef45fc2c1af46510d9e8122e6329742fed5fdb
Forrest Fleming <ffleming@gmail.com>
2017-03-25 19:08:08 -0700

    move methods only used in handshake to Handshake

--- lib/sticky_elephant/connection/base.rb
+++ lib/sticky_elephant/connection/base.rb
@@ -15,33 +15,9 @@ module StickyElephant
         logger.send(level, socket.remote_address.ip_address) { msg }
       end
 
-      def write_key_data
-        socket.write("K")
-        socket.write(
-          with_length_bytes(
-            [pid].pack('N') +
-            [key].pack('N')
-          )
-        )
-      end
-
-      def write_parameter_status(key, value)
-        to_write = "S" + with_length_bytes("#{key}\x00#{value}\x00")
-        socket.write(to_write)
-        log(msg: "Wrote kv #{to_write}", level: :debug)
-      end
-
       def with_length_bytes(val)
         length = 4 + val.bytesize
         [length].pack("N") + val.to_s
-      end
-
-      def pid
-        666
-      end
-
-      def key
-        0xEFBEADDE
       end
     end
   end
--- lib/sticky_elephant/connection/handshake.rb
+++ lib/sticky_elephant/connection/handshake.rb
@@ -23,6 +23,7 @@ module StickyElephant
         write_parameter_status("session_authorization", hash[:user])
         write_parameter_status("standard_conforming_strings", "on")
         write_parameter_status("TimeZone", "US/Pacific")
+        write_key_data
         response_string = [0x4b, 0x00, 0x00, 0x00, 0x0c, 0x00, 0x01, 0x56,
                            0x7b, 0x27, 0x50, 0xb7, 0x0c, 0x5a, 0x00, 0x00,
                            0x00, 0x05, 0x49].map(&:chr).join
@@ -53,6 +54,31 @@ module StickyElephant
         socket.write("R")
         socket.write(with_length_bytes("\x00\x00\x00\x00"))
       end
+
+      def write_key_data
+        socket.write("K")
+        socket.write(
+          with_length_bytes(
+            [pid].pack('N') +
+            [key].pack('N')
+          )
+        )
+      end
+
+      def pid
+        666
+      end
+
+      def key
+        0xEFBEADDE
+      end
+
+      def write_parameter_status(key, value)
+        to_write = "S" + with_length_bytes("#{key}\x00#{value}\x00")
+        socket.write(to_write)
+        log(msg: "Wrote kv #{to_write}", level: :debug)
+      end
+
     end
   end
 end
