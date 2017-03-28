7288b9398b1866d25d0a5f1a7989a50f19542da8
Forrest Fleming <forrest.fleming@procore.com>
2017-03-28 16:35:08 -0700

    log wrapper
    pass level methods through

---
+++ lib/sticky_elephant/elephant_logger.rb
@@ -0,0 +1,26 @@
+module StickyElephant
+  class ElephantLogger
+    def initialize
+      @text = Logger.new("sticky_elephant.log")
+    end
+
+    %i(debug info warn error fatal unknown).each do |level|
+      define_method(level) do |*args, &block|
+        @text.send(level, *args, &block)
+      end
+    end
+
+    def level=(lev)
+      text.level = lev
+    end
+
+    def level
+      text.level
+    end
+
+    private
+
+    attr_reader :text
+
+  end
+end
