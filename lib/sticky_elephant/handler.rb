6460b53b3cc56df033c74307767017fdfea67918
Forrest Fleming <ffleming@gmail.com>
2017-04-28 15:25:15 -0700

    use payload handler-detector

--- lib/sticky_elephant/handler.rb
+++ lib/sticky_elephant/handler.rb
@@ -1,23 +1,7 @@
 module StickyElephant
   module Handler
     def self.for(payload, socket: , logger: )
-      case payload[0]
-      when 0
-        if SSLRequest.validates?(payload)
-          SSLRequest.new(payload, socket: socket, logger: logger)
-        elsif Handshake.validates?(payload)
-          Handshake.new(payload, socket: socket, logger: logger)
-        else
-          Error.new(payload, socket: socket, logger: logger)
-        end
-      when 'Q'.ord
-        Query.new(payload, socket: socket, logger: logger)
-      when 'X'.ord
-        Null.new(payload, socket: socket, logger: logger)
-      else
-        logger.send(:debug, socket.remote_address.ip_address) { "Unknown input: #{payload}" }
-        Null.new(payload, socket: socket, logger: logger)
-      end
+      StickyElephant::Payload.new(payload).handler.new(payload, socket: socket, logger: logger)
     end
   end
 end
