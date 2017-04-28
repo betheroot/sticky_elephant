4ff8606037f54b4cc87e8182fc330ac54f0584c8
Forrest Fleming <ffleming@gmail.com>
2017-04-28 13:52:58 -0700

    remove default broken spec

--- spec/sticky_elephant_spec.rb
+++ spec/sticky_elephant_spec.rb
@@ -4,8 +4,4 @@ describe StickyElephant do
   it "has a version number" do
     expect(StickyElephant::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
