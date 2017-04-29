d6f17257cd82111d7f2eed88279a9317792819a2
Forrest Fleming <ffleming@gmail.com>
2017-04-29 09:15:44 -0700

    more specs

--- spec/payload_spec.rb
+++ spec/payload_spec.rb
@@ -17,31 +17,43 @@ end
 
 describe StickyElephant::Payload do
   let(:query) { StickyElephant::Payload.new(fixture('query.valid')) }
-  let(:invalid_query_length) { StickyElephant::Payload.new(fixture('query.invalid')) }
+  let(:invalid_query) { StickyElephant::Payload.new(fixture('query.invalid')) }
+  let(:ssl_request) { StickyElephant::Payload.new(fixture('ssl_request.valid'))}
+  let(:invalid_ssl_request) { StickyElephant::Payload.new(fixture('ssl_request.invalid'))}
+  let(:quit) { StickyElephant::Payload.new(fixture('quit.valid'))}
 
   let(:invalid_payloads) do
     [
-      invalid_query_length,
+      invalid_query,
+      invalid_ssl_request,
       StickyElephant::Payload.new([88]),
       StickyElephant::Payload.new([88, 0, 0, 0, 3]),
     ]
   end
 
   describe("#to_s") do
-    it do
-      expect(query.to_s).to eq 'QUERY: \'select * from camels;\''
+    context "Query" do
+      it "Returns the appropriate string" do
+        expect(query.to_s).to eq 'QUERY: \'select * from camels;\''
+      end
     end
   end
 
   describe "#type" do
-    it "correctly sets :query" do
-      expect(query.type).to eq :query
+    it "returns the correct symbol" do
+      aggregate_failures do
+        expect(query.type).to eq :query
+        expect(ssl_request.type).to eq :ssl_request
+        expect(quit.type).to eq :quit
+      end
     end
 
-    it "correctly sets :invalid" do
-      aggregate_failures do
-        invalid_payloads.each do |payload|
-          expect(payload.type).to eq :invalid
+    context "Invalid requests" do
+      it "correctly sets :invalid" do
+        aggregate_failures do
+          invalid_payloads.each do |payload|
+            expect(payload.type).to eq :invalid
+          end
         end
       end
     end
