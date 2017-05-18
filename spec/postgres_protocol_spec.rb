24f990e0ad8e1e4a3cfe79b5c07ff8a731a9649c
Forrest Fleming <forrest.fleming@procore.com>
2017-05-18 13:30:32 -0700

    unit tests for command_complete

--- spec/postgres_protocol_spec.rb
+++ spec/postgres_protocol_spec.rb
@@ -12,5 +12,24 @@ describe StickyElephant::PostgresProtocol do
     expected = fixture("row-description")
     expect(protocol.row_description('version').bytes).to eq expected
   end
+
+  describe "#command_complete" do
+    command_complete_tests =
+      [
+        {
+          args: [:select, 1],
+          expected: "C\x00\x00\x00\rSELECT 1\x00"
+        },
+        {
+          args: [:set],
+          expected: "C\x00\x00\x00\x08SET\x00"
+        }
+    ]
+    command_complete_tests.each do |h|
+      it "Should respond correctly given arguments (#{h[:args].join(', ')})" do
+        expect(protocol.command_complete(*h[:args])).to eq h[:expected]
+      end
+    end
+  end
 end
 
