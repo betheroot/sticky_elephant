52a6c1585a64b19c50236827cd8b87df87ef85b7
Forrest Fleming <forrest.fleming@procore.com>
2017-05-18 14:37:08 -0700

    set query tests

--- spec/postgres_simulator_spec.rb
+++ spec/postgres_simulator_spec.rb
@@ -13,4 +13,24 @@ describe StickyElephant::PostgresSimulator do
     expected = fixture("current-setting-response")
     expect(conf_sim.response.bytes).to eq expected
   end
+
+  it "Should give the correct response to SET queries" do
+    set_query_tests = [
+      "set client_encoding to gbk;",
+      "set client_encoding\nto gbk\n;",
+      "set client_encoding = gbk;",
+      "set client_encoding= gbk;",
+      "set client_encoding=gbk;",
+      "set client_encoding =gbk;",
+      "set client_encoding ='gbk';",
+    ]
+
+    aggregate_failures do
+      expected = "S\x00\x00\x00\x19client_encoding\x00gbk;\x00C\x00\x00\x00\bSET\x00Z\x00\x00\x00\x05I".force_encoding("ASCII-8BIT")
+      set_query_tests.each do |test_str|
+        expect(StickyElephant::PostgresSimulator.new(test_str).response).to eq expected
+      end
+    end
+
+  end
 end
